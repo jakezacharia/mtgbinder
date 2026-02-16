f87c019d90bf2642cec62e6a169ec5c9cb93f037
Jacob Zacharia <jacobzach@pm.me>
2026-02-16 01:27:02 -0800

    added double sided card image parsing from json blob

--- services/catalog_service/app/main.py
+++ services/catalog_service/app/main.py
@@ -16,7 +16,7 @@ async def root():
 @app.get("/card_test")
 def card_test():
     url = "https://api.scryfall.com/cards/named"
-    params = {"exact": "dark ritual", "set": "LEA" }
+    params = {"exact": "Sephiroth, Fabled SOLDIER" }
     headers = {"User-Agent": "mtg-binder dev"}
     
     resp = httpx.get(url, params=params, headers=headers, timeout=10.0)
